57fe17042ab7c2befeec426be8199da87dd15320
DESKTOP-F9CES1J\HOME <thieumy7911@gmail.com>
2025-01-18 17:51:29 +0700

    no message

--- homework1.py
+++ homework1.py
@@ -1,4 +1,24 @@
 from guizero import Text, App, Picture
 app = App(title="chào mừng đến guizero", width=500, height=500, layout="auto", bg="light blue")
-text = Text(app, "Hãy lập trình Gui cùng guizero", color="green", size= 18)
+text = Text(app, "Hãy lập trình Gui cùng guizero", color="green", size=18)
+picture = Picture(app, "screenshot_1736682062.png", width=300, height=300)
 app.display()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
